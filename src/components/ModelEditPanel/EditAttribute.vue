580dd29d598d50bc7171237774ed421743486271
答案 <1259558183@qq.com>
2023-07-05 09:12:45 +0000

    update src/components/ModelEditPanel/EditAttribute.vue.
    Signed-off-by: 答案 <1259558183@qq.com>

--- src/components/ModelEditPanel/EditAttribute.vue
+++ src/components/ModelEditPanel/EditAttribute.vue
@@ -6,55 +6,70 @@
     </div>
     <!-- 模型旋转 -->
     <div class="options" :class="optionDisabled">
-      <div class="option" :style="{ justifyContent: 'space-between' }">
+      <div class="option space-between">
         <el-space>
-          <el-icon><Setting /></el-icon>
+          <el-icon>
+            <Setting />
+          </el-icon>
           <span> 模型轴旋转 </span>
         </el-space>
         <el-button type="text" icon="Refresh" @click="onResultRotate"> 重置 </el-button>
       </div>
       <div class="option">
-        <el-button
-          type="info"
-          icon="RefreshRight"
-          @click="onSetRotateOnAxis('x', 'right')"
-        />
+        <el-button type="info" icon="RefreshRight" @click="onSetRotateOnAxis('x', 'right')" />
         <el-button type="text">X轴</el-button>
-        <el-button
-          type="info"
-          icon="RefreshLeft"
-          @click="onSetRotateOnAxis('x', 'left')"
-        />
+        <el-button type="info" icon="RefreshLeft" @click="onSetRotateOnAxis('x', 'left')" />
       </div>
       <div class="option">
-        <el-button
-          type="info"
-          icon="RefreshRight"
-          @click="onSetRotateOnAxis('y', 'right')"
-        />
+        <el-button type="info" icon="RefreshRight" @click="onSetRotateOnAxis('y', 'right')" />
         <el-button type="text">Y轴</el-button>
-        <el-button
-          type="info"
-          icon="RefreshLeft"
-          @click="onSetRotateOnAxis('y', 'left')"
-        />
+        <el-button type="info" icon="RefreshLeft" @click="onSetRotateOnAxis('y', 'left')" />
       </div>
       <div class="option">
-        <el-button
-          type="info"
-          icon="RefreshRight"
-          @click="onSetRotateOnAxis('z', 'right')"
-        />
+        <el-button type="info" icon="RefreshRight" @click="onSetRotateOnAxis('z', 'right')" />
         <el-button type="text">Z轴</el-button>
-        <el-button
-          type="info"
-          icon="RefreshLeft"
-          @click="onSetRotateOnAxis('z', 'left')"
-        />
+        <el-button type="info" icon="RefreshLeft" @click="onSetRotateOnAxis('z', 'left')" />
       </div>
-      <div class="option" :style="{ justifyContent: 'space-between' }">
+      <!-- 模型位置 -->
+      <div class="option space-between">
         <el-space>
-          <el-icon><Eleme /></el-icon>
+          <el-icon>
+            <Location />
+          </el-icon>
+          <span> 模型位置 </span>
+        </el-space>
+        <el-button type="text" icon="Refresh" @click="onResultPosition"> 重置 </el-button>
+      </div>
+      <div class="option">
+        <div class="grid-txt">
+          <el-button type="text">X 轴</el-button>
+        </div>
+        <div class="grid-silder">
+          <el-slider @change="onSetPosition" v-model="config.positionX" :min="-10" :max="10" :step="0.01" />
+        </div>
+      </div>
+      <div class="option">
+        <div class="grid-txt">
+          <el-button  type="text">Y 轴</el-button>
+        </div>
+        <div class="grid-silder">
+          <el-slider @change="onSetPosition" v-model="config.positionY" :min="-10" :max="10" :step="0.01" />
+        </div>
+      </div>
+      <div class="option">
+        <div class="grid-txt">
+          <el-button type="text">Z 轴</el-button>
+        </div>
+        <div class="grid-silder">
+          <el-slider @change="onSetPosition" v-model="config.positionZ"  :min="-10" :max="10" :step="0.01" />
+        </div>
+      </div>
+      <!-- 模型骨架 -->
+      <div class="option space-between">
+        <el-space>
+          <el-icon>
+            <Eleme />
+          </el-icon>
           <span> 模型骨架 </span>
         </el-space>
         <el-switch v-model="config.skeletonHelper" @change="onSetModelHelper" />
@@ -62,9 +77,11 @@
     </div>
     <!-- 网格辅助线 -->
     <div class="options" :class="optionDisabled">
-      <div class="option" :style="{ justifyContent: 'space-between' }">
+      <div class="option space-between">
         <el-space>
-          <el-icon><Grid /></el-icon>
+          <el-icon>
+            <Grid />
+          </el-icon>
           <span> 网格辅助线 </span>
         </el-space>
         <el-switch v-model="config.gridHelper" @change="onChangeGridHelper" />
@@ -74,83 +91,69 @@
       <!-- x轴 -->
       <div class="option">
         <div class="grid-txt">
-          <el-button type="text">X轴</el-button>
+          <el-button type="text">X 轴</el-button>
         </div>
         <div class="grid-silder">
-          <el-slider
-            @change="onChangeGridHelper"
-            v-model="config.x"
-            :min="-2"
-            :max="2"
-            :step="0.01"
-          />
+          <el-slider @change="onChangeGridHelper" v-model="config.x" :min="-2" :max="2" :step="0.01" />
         </div>
       </div>
       <!-- Y轴 -->
       <div class="option">
         <div class="grid-txt">
-          <el-button type="text">Y轴</el-button>
+          <el-button type="text">Y 轴</el-button>
         </div>
         <div class="grid-silder">
-          <el-slider
-            @change="onChangeGridHelper"
-            v-model="config.y"
-            :min="-2"
-            :max="2"
-            :step="0.01"
-          />
+          <el-slider @change="onChangeGridHelper" v-model="config.y" :min="-2" :max="2" :step="0.01" />
         </div>
       </div>
       <!-- Z轴 -->
       <div class="option">
         <div class="grid-txt">
-          <el-button type="text">Z轴</el-button>
+          <el-button type="text">Z 轴</el-button>
         </div>
         <div class="grid-silder">
-          <el-slider
-            @change="onChangeGridHelper"
-            v-model="config.z"
-            :min="-2"
-            :max="2"
-            :step="0.01"
-          />
+          <el-slider @change="onChangeGridHelper" v-model="config.z" :min="-2" :max="2" :step="0.01" />
         </div>
       </div>
       <!-- 网格数量/大小 -->
       <div class="option">
         <div>
           <el-button type="text">大小</el-button>
-          <el-input-number
-            :controls="true"
-            @change="onChangeGridHelperSize"
-            v-model="config.size"
-            :min="1"
-            :max="40"
-          />
+          <el-input-number :controls="true" @change="onChangeGridHelperSize" v-model="config.size" :min="1" :max="40" />
         </div>
         <div>
           <el-button type="text">分割数</el-button>
-          <el-input-number
-            :controls="true"
-            @change="onChangeGridHelperSize"
-            v-model="config.divisions"
-            :min="1"
-            :max="40"
-          />
+          <el-input-number :controls="true" @change="onChangeGridHelperSize" v-model="config.divisions" :min="1"
+            :max="40" />
         </div>
       </div>
       <!-- 网格颜色 -->
       <div class="option">
         <el-space>
           <el-button type="text">网格颜色</el-button>
-          <el-color-picker
-            color-format="hex"
-            :predefine="predefineColors"
-            @change="onChangeGridHelper"
-            @active-change="activeChangeColor"
-            v-model="config.color"
-          />
+          <el-color-picker color-format="hex" :predefine="predefineColors" @change="onChangeGridHelper"
+            @active-change="activeChangeColor" v-model="config.color" />
         </el-space>
+      </div>
+    </div>
+    <!-- 坐标轴辅助线 -->
+    <div class="options" :class="optionDisabled">
+      <div class="option space-between">
+        <el-space>
+          <el-icon>
+            <Rank />
+          </el-icon>
+          <span> 坐标轴辅助线 </span>
+        </el-space>
+        <el-switch v-model="config.axesHelper" @change="onChangeAxesHelper" />
+      </div>
+      <div class="option" :class="axesDisabled">
+        <div class="grid-txt">
+          <el-button type="text">轴长度</el-button>
+        </div>
+        <div class="grid-silder">
+          <el-slider @change="onChangeAxesHelper" v-model="config.axesSize" :min="1" :max="10" :step=".2" />
+        </div>
       </div>
     </div>
   </div>
@@ -169,9 +172,14 @@ const config = reactive({
   x: 0,
   y: -0.2,
   z: -0.1,
+  positionX:0,
+  positionY:0,
+  positionZ:0,
   divisions: 10,
   size: 4,
   color: "#1395E6",
+  axesHelper: false,
+  axesSize: 1.8,
 });
 const predefineColors = PREDEFINE_COLORS;
 const optionDisabled = computed(() => {
@@ -181,12 +189,16 @@ const gridDisabled = computed(() => {
   const { visible, gridHelper } = config;
   return gridHelper && visible ? "" : "disabled";
 });
+const axesDisabled = computed(() => {
+  const { visible, axesHelper } = config;
+  return axesHelper && visible ? "" : "disabled";
+});
 const state = reactive({
   modelApi: computed(() => {
     return store.state.modelApi;
   }),
 });
-const onChangeAttributeSwitch = () => {};
+const onChangeAttributeSwitch = () => { };
 // 设置模型轴角度
 const onSetRotateOnAxis = (type, direction) => {
   const flag = direction == "right" ? true : false;
@@ -196,6 +208,18 @@ const onSetRotateOnAxis = (type, direction) => {
 const onResultRotate = () => {
   state.modelApi.onResultModelRotateOnAxis();
 };
+
+// 设置模型位置
+const onSetPosition = () => {
+  state.modelApi.onSetModelPosition(config)
+};
+// 重置模型位置
+const onResultPosition =()=>{
+  config.positionX=0
+  config.positionY=0
+  config.positionZ=0
+  state.modelApi.onResultModelPosition();
+}
 
 const onSetModelHelper = () => {
   state.modelApi.onSetModelHelper(config.skeletonHelper);
@@ -212,12 +236,19 @@ const onChangeGridHelper = () => {
 const onChangeGridHelperSize = () => {
   state.modelApi.onSetModelGridHelperSize(config);
 };
+
+//设置坐标轴辅助线
+const onChangeAxesHelper = () => {
+  state.modelApi.onSetModelAxesHelper(config);
+
+}
 </script>
 
 <style lang="scss">
 .grid-style {
   min-width: 110px;
 }
+
 .grid-silder {
   flex: 5;
   padding-left: 10px;
