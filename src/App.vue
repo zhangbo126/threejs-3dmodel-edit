1cc4058abc11c4f90d68e147b2342401baaeea6b
张博 <bozhangeq@isoftstone.com>
2023-08-14 11:14:22 +0800

    fix:更新打包文件

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-config-provider >
-      <router-view />
-  </el-config-provider>
-</template>
-<script setup>
-</script>
-<style lang="less">
-</style>
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-const publicPath =  process.env.NODE_ENV =='production'? '/three.js3d/':'/'
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath,
-  lintOnSave: false,
-  assetsDir: 'static',
-  productionSourceMap: false,
-})
