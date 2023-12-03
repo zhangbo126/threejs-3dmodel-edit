0933bfa3bb63bcd51eaf0e61dc3fcf64f3c9a4fb
ZHANG_666 <1259558183@qq.com>
2023-12-03 14:11:23 +0800

    refactor:ts分支版本初次提交

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
