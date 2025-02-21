9e0be46386df243aa6a0219146f25bf8799e2193
ZHANG_666 <1259558183@qq.com>
2025-02-21 13:22:28 +0800

    docs: 更新项目文档

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <el-config-provider>
     <router-view />
+    <MessageTipDialog ref="messageTipDialog" />
   </el-config-provider>
 </template>
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import MessageTipDialog from "@/components/MessageTipDialog/index.vue";
+const messageTipDialog = ref(null);
+onMounted(() => {
+  messageTipDialog.value.showDialog();
+});
+</script>
